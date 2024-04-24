26895cf4bdfb9a546706e97083f9fc7b8c95ee5c
Lehlomela Mokoena <lehlomela.mokoena@momentum.co.za>
2024-04-24 19:22:44 +0200

    auto commit message Wed 24 Apr 2024 19:22:44 SAST

--- move_files_to_folders.py
+++ move_files_to_folders.py
@@ -31,10 +31,10 @@ def search_files_for_tags(dir):
                         move_file_to_directory(filename, folder_name)
                         break
     
-    destination_folder = './Notes/images/'
+    destination_folder = f'{dir}/Notes/images/'
     if(os.path.exists(destination_folder)):
         shutil.rmtree(destination_folder)
-    shutil.copytree('./images', destination_folder)
+    shutil.copytree(f'{dir}/images', destination_folder)
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
